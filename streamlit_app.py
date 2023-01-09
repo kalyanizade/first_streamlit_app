cb49a4c5c9f6a7e9fd305255a72746db92abfc34
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-09 18:15:07 +0530

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My parents new Healthy Dinner')
+streamlit.title('My parents new Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry outmeal')
+streamlit.text('Kale ,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
