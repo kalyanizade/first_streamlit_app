3d9e52a6511b314f995c5bb02a8289cb2f40484f
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-22 22:56:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
@@ -31,13 +34,11 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-
-
 #New section to display fruitvice API response
 
- #streamlit.header("Fruityvice Fruit Advice!")
+#streamlit.header("Fruityvice Fruit Advice!")
 
 
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
