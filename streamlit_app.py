37784258c171830cc0fe43c2363d2a1f9bec102c
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-22 22:49:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.header('🍔Breakfast menu🍔')
 streamlit.text('🍱Omega 3 & Blueberry outmeal')
 streamlit.text('🫘Kale ,Spinach & Rocket Smoothie')
 streamlit.text('🍳🥚Hard-Boiled Free-Range Egg')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
@@ -12,23 +13,22 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 
+
+
+
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-streamlit.dataframe(my_fruit_list)
-
-
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
-
 # Display the table on the page.
+
 streamlit.dataframe(fruits_to_show)
+
+
+
+
+
+
+
 
 
 
