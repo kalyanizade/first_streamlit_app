99e6bd92e6043bcd9ade5efee8e715380e462ae7
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-22 23:24:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,15 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 
+
+
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
@@ -58,9 +67,9 @@ streamlit.dataframe(fruityvice_normalized)
 #New section to display fruitvice API response
 #streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#streamlit.write('The user entered ', fruit_choice)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
