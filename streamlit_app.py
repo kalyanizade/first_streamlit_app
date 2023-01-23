3f88fd0d8de750962448efcb05deefda435b4318
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-24 00:17:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,10 +85,10 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
+my_data_rows = my_cur.fetchone()
 
-streamlit.header("The fruit lode list contains:")
-streamlit.dataframe(my_data_rows)
+streamlit.text("The fruit lode list contains:")
+streamlit.text(my_data_row)
 
 
 
