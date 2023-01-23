b0b7259bd374bdc17c64e53eb11c3b0944d3ff8e
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-24 00:00:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,8 +87,8 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 
-streamlit.text("The fruit lode list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit lode list contains:")
+streamlit.dataframe(my_data_row)
 
 
 
