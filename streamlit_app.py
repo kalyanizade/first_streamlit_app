5df256455fbf5eed72437a70ae5063086711b852
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-30 17:38:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit
+import pandas
+import requests
+import requests
+import requests
+import snowflake.connector
+import urllib.error import URLerror
+
+
+
 streamlit.title('My parents new Healthy Diner')
 streamlit.header('🍔Breakfast menu🍔')
 streamlit.text('🍱Omega 3 & Blueberry outmeal')
@@ -7,7 +16,7 @@ streamlit.text('🍳🥚Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -45,13 +54,13 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
 
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -59,7 +68,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 #take the json version of the response and normalize it
@@ -76,7 +85,7 @@ streamlit.dataframe(fruityvice_normalized)
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
-import snowflake.connector
+
 
 
 
