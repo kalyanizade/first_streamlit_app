d5a81d0e54e23f2d90d48a359e7b43315b755d71
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-30 18:10:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,9 +77,9 @@ streamlit.dataframe(fruityvice_normalized)
 #create the header repitable code called(a function)
 
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/++ fruit_choice")
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
- return(fruityvice_normalized)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
