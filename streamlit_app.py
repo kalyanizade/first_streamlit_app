6c923d11632373d33f75bd8006933a444063e81c
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-30 22:56:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,18 +118,18 @@ if streamlit.button('Get fruit load list'):
 
     
     
-    #  streamlit.header("Fruityvice Fruit Advice!")
-#try:
-#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
- # if not fruit_choice:
-  #  streamlit.error("Please select a fruit for information")
- # else:
-  #  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/")
-  #  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  #streamlit.dataframe(fruityvice_normalized)
+      streamlit.header("Fruityvice Fruit Advice!")
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+   streamlit.error("Please select a fruit for information")
+ else:
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/")
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   streamlit.dataframe(fruityvice_normalized)
    
-#except URLError as e:
- # streamlit.error();
+except URLError as e:
+streamlit.error();
 
   
   
