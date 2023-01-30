283f156355d519649a97beaf08e06617dbb2e1fb
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-30 18:01:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,6 +73,25 @@ streamlit.dataframe(fruityvice_normalized)
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/")
 
 
+
+#create the header repitable code called(a function)
+
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/++ fruit_choice")
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+ return(fruityvice_normalized)
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit for information")
+    else:
+      back_from_function=get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
+
+
+
+
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
