80a7ecf25969dca89994a67a8721fc6053c9b935
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2023-01-30 18:18:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,6 +91,9 @@ try:
       back_from_function=get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
 
+      except URLError as e:
+  streamlit.error();
+
       
 
     #  streamlit.header("Fruityvice Fruit Advice!")
@@ -103,8 +106,8 @@ try:
   #  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   #streamlit.dataframe(fruityvice_normalized)
    
-except URLError as e:
-  streamlit.error();
+#except URLError as e:
+ # streamlit.error();
 
   
   
